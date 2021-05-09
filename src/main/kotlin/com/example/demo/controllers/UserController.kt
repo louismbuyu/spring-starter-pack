cc4f47eb57fb34c7e6cbb9d9699354916ea1f8a2
louismbuyu <louisnelsonm@gmail.com>
2021-05-09 14:26:29 -0500

    basic code

---
+++ src/main/kotlin/com/example/demo/controllers/UserController.kt
@@ -0,0 +1,15 @@
+package com.example.demo.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class UserController {
+
+
+    @GetMapping
+    fun helloWorld() {
+
+    }
+}
